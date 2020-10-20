cca0f99b0adaed90cab6b6f25601ac8ae7e13a66
John Eriksson <johneriksson2000@gmail.com>
2020-10-20 13:03:04 -0400

    Login screen and registration done

--- app.py
+++ app.py
@@ -1,32 +1,95 @@
 from tkinter import *
+import os
+def register():
+    count = len(open("users.txt").readlines())
+    if count>=10:
+        Label(screennew, text = "Too many users",fg="red").pack()
+    else:
+        nameinfo=username.get()
+        passinfo=password.get()
+        f =open("users.txt","a")
+        f.write(nameinfo+" "+passinfo+'\n')
+        f.close()
+        usernameentered.delete(0, END)
+        passwordentered.delete(0, END)
+        Label(screennew, text = "Successfully registered.", fg="green").pack()
 
-main_screen = Tk()  
 
-def main_account_screen():
+def newuser():
+    global username
+    global password
+    global screennew
+    global passwordentered
+    global usernameentered
     
-    main_screen = Tk()   
-    main_screen.geometry("300x250") 
-    main_screen.title("DMC Login") 
-
-Label(text="Choose Login Or Register", bg="red", width="300", height="4", font=("Times New Roman", 13)).pack() 
-Label(text="").pack() 
-
-Button(text="Login", height="3", width="15").pack() 
-Label(text="").pack() 
-
-Button(text="Register", height="3", width="15").pack()
- 
-main_screen.mainloop() 
-
-main_account_screen() 
-
-def registration():
-    register = Toplevel(main_screen)
-    register.title("Register new user")
-    register.geometry("500x500")
-    user = StringVar()
+    screennew = Toplevel(screen)
+    screennew.title("Registration Info")
+    screennew.geometry("500x250")
+    username = StringVar()
     password = StringVar()
-    Label(register,text = "Enter the users details below:", bg="red").pack()
-    Label(register, text="").pack()
-    username = Label(register, text="Username *")
-    username.pack()
+    Label(screennew,text = "UserID").pack()
+    usernameentered = Entry(screennew,textvariable = username)
+    usernameentered.pack()
+    Label(screennew,text = "Password").pack()
+    passwordentered = Entry(screennew,textvariable = password)
+    passwordentered.pack()
+    Button(screennew,text = "Complete registration", command=register).pack()
+
+def completelogin():
+    nameinfo=username.get()
+    passinfo=password.get()
+    f =open("users.txt","r")
+    x = f.readlines()
+    for i in x:
+        x=i.split()
+        print(x[0])
+        print(nameinfo)
+        if str(x[0]) == str(nameinfo) and  str(x[1]) == str(passinfo):
+                Label(loginscreen,text="Success!", fg="green").pack()
+                return
+    Label(loginscreen,text="Login unsuccessful.", fg="red").pack()
+
+    f.close()
+    usernameentered.delete(0, END)
+    passwordentered.delete(0, END)
+    
+
+
+
+def login():
+    global username
+    global password
+    global loginscreen
+    global passwordentered
+    global usernameentered
+    loginscreen = Toplevel(screen)
+    loginscreen.title("Login")
+    loginscreen.geometry("500x250")
+    username = StringVar()
+    password = StringVar()
+    Label(loginscreen,text = "UserID").pack()
+    usernameentered = Entry(loginscreen,textvariable = username)
+    usernameentered.pack()
+    Label(loginscreen,text = "Password").pack()
+    passwordentered = Entry(loginscreen,textvariable = password)
+    passwordentered.pack()
+    Button(loginscreen,text = "Login", command=completelogin).pack()
+
+
+
+def main_screen():
+    global screen
+    screen = Tk()
+    screen.geometry("500x250")
+    screen.title("DCM Main")
+    Label(text = "Welcome to DCM", font = ("Times",13), bg="red").pack()
+    Label(text = "        ",bg  ="red").pack()
+    Label(text = "        ",bg  ="red").pack()
+    Button(text = "Register new user", command = newuser).pack()
+    Label(text = "        ",bg  ="red").pack()
+    Button(text="Login existing user", command = login).pack()
+    screen.configure(background='red')
+    screen.mainloop()
+
+main_screen()
+    
